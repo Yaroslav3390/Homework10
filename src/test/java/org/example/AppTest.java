2327c3c565b4464eb2ab1500b1d905866a47b8e5
Yaroslav3390 <yaroslavkutnenko@gmail.com>
2023-07-16 18:18:46 +0300

    homework10

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -9,31 +14,114 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class AppTest {
 
+    @ParameterizedTest
+    @ValueSource(ints = {1, 4, 5})
+
+    public void parametrsTestGetPositive(int id) {
+
+        given()
+                .when()
+                .log()
+                .all()
+                .get("http://51.250.6.164:8080/test-orders/{id}", id)
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK);
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {3, 4, 5})
+
+    public void parametrsTestGetNegative(int id) {
+
+
+          given()
+                .when()
+                .log()
+                .all()
+                .get("http://51.250.6.164:8080/test-orders/{id}", id)
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST);
+    }
+
+
     @Test
-    public void checkTwoValuesEquals(){
 
-        //
-        //
+    public void TestPostPositive() {
+        String requestBody = "{\n" +
+                "  \"status\": \"OPEN\",\n" +
+                "  \"courierId\": 0,\n" +
+                "  \"customerName\": \"string\",\n" +
+                "  \"customerPhone\": \"string\",\n" +
+                "  \"comment\": \"string\",\n" +
+                "  \"id\": -1340938102087703600\n" +
+                "}";
 
-        assertEquals( 3 , 3);
+
+        given()
+                .when()
+                .header("Content-Type", "application/json")
+                .body(requestBody)
+                .log()
+                .all()
+                .post("http://51.250.6.164:8080/test-orders")
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK);
     }
 
     @Test
-    public void checkTwoValuesNotEquals(){
-        assertNotEquals( 3 , 4);
+
+    public void TestPostNegative() {
+        String requestBody = "{\n" +
+                "  \"status\": \"OPEN\",\n" +
+                "  \"courierId\": 0,\n" +
+                "  \"customerName\": \"string\",\n" +
+                "  \"customerPhone\": \"string\",\n" +
+                "  \"comment\": \"string\",\n" +
+                "  \"id\": -1340938102087703600\n" +
+                "}";
+
+
+        given()
+                .when()
+                .header("Content-Type", "application/xml")
+                .body(requestBody)
+                .log()
+                .all()
+                .post("http://51.250.6.164:8080/test-orders")
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_UNSUPPORTED_MEDIA_TYPE);
     }
+
+
+
+
+
+
+
 
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( 3 == 3 );
+
+    public void testGetPositiveOrderStatus() {
+
+       String status = given()
+                .when()
+                .log()
+                .all()
+                .get("http://51.250.6.164:8080/test-orders/5")
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .path("status");
+       Assertions.assertEquals("OPEN", status);
+
     }
-
-    @Test
-    public void shouldAnswerWithFalse()
-    {
-        assertFalse( 4 < 2 );
-    }
-
-
 }
